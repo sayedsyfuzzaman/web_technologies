cd0b046b1a127a0c88e97ea75cb555bc2bc457fe
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2021-10-20 12:38:29 +0600

    Global Dashboard Template

---
+++ Portal Template/dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="styles\style.css">
+    <title>Dashboard</title>
+</head>
+
+
+<body>
+    <div class="split-screen">
+        <?php include 'portal_header.php'; ?>
+        <?php include 'navigation_bar.php' ?>
+        <div class="portal-body">
+
+            <!-- Write your code here -->
+
+            
+
+            <!-- End your code here -->
+            
+        </div>
+    </div>
+    <?php include 'portal_footer.php'; ?>
+    </div>
+</body>
+
+</html>
